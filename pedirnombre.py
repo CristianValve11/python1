4c1c032705e4e75def204345671c2460eca28927
Cristian Valverde <cvalosu636@g.educaand.es>
2024-10-14 17:18:03 +0200

    ejemplo commit2

--- pedirnombre.py
+++ pedirnombre.py
@@ -2,3 +2,17 @@
 nombre = input("Introduce tu nombre: ")
 print("¡Hola" + nombre)
 print("Me alegro de conocerle," + nombre)
+
+
+
+nombre = input("Introduce tu nombre: ")
+nombreMayus = nombre.upper() #upper para ponerlo en Mayusculas
+numeroLetras = len(nombre) #len da la longitud de ka cadena intrducida
+
+print(f"Tu nombre es: {nombreMayus}")
+print(f"Número de caracteres: {numeroLetras}")
+
+# Se repite nombre en bucle
+#segun número de letras (for each)
+for i in range(numeroLetras):
+    print(nombre)
