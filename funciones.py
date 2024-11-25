879f14741ea7728478cf155057a4c370b71d8532
Cristian Valverde <cvalosu636@g.educaand.es>
2024-11-25 17:07:24 +0100

    Ejercicios2Boletin

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def main():
+    print("Pares e Impares")
+    num1 = int(input("Escriba numero 1"))
+    num2 = int(input("Escriba numero 2"))
+    if num2 < num1:
+        print(f"Le he pedudo un numero mayor que {num1}")
+    else:
+        
+        #logica para continuar la funcion
+        pass
+
+
+def otra(): #definir una función, llamada otra
+
+    pass #con esto, nuestra función otra, pasará hacia delante. Ideal para implementar algo mas adelante
---
+++ listas.py
@@ -0,0 +1,8 @@
+lista= [5,4,3,3]
+lista.append(7) #añadir el 7
+lista.sort() #ordenar lista
+print(lista)
+print(lista.count(3)) #cuenta cuantas veces aparece el 3: 2 veces
+
+print(lista.index(5)) #devuelve el index de 5, que será 3 (cuarta posicion en array)
+
